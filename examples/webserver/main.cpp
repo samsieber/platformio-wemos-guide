6afe26a384f9fafe564531cde59dc6029a2d81f5
Sam Sieber <samsieber@users.noreply.github.com>
2016-08-06 10:17:34 -0600

    Update synchronous server example.

--- examples/webserver/main.cpp
+++ examples/webserver/main.cpp
@@ -17,15 +17,21 @@ MDNSResponder mdns;
 ESP8266WebServer server(80);
 
 const int led = BUILTIN_LED;
+uint8_t LED_ON = LOW;
+uint8_t LED_OFF = HIGH;
 
+// This a handler we can attach to our server
+// Since the server is synchronous, the api is to write responses on the server object
+// Here we blink the LED on whilst processing a request
 void handleRoot() {
-  digitalWrite(led, 1);
+  digitalWrite(led, LED_ON);
   server.send(200, "text/plain", "hello from esp8266!");
-  digitalWrite(led, 0);
+  digitalWrite(led, LED_OFF);
 }
 
+// All request info is read from the server object as well.
 void handleNotFound(){
-  digitalWrite(led, 1);
+  digitalWrite(led, LED_ON);
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
@@ -38,12 +44,12 @@ void handleNotFound(){
     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
   server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
+  digitalWrite(led, LED_OFF);
 }
 
 void setup(void){
   pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
+  digitalWrite(led, LED_OFF);
   // Note the serial rate - if you don't match it when connecting you'll get garbage.
   Serial.begin(115200);
   WiFi.begin(ssid, password);
@@ -66,6 +72,7 @@ void setup(void){
     Serial.println("MDNS responder started");
   }
 
+  
   server.on("/", handleRoot);
 
   server.on("/inline", [](){
@@ -79,5 +86,6 @@ void setup(void){
 }
 
 void loop(void){
+  // This seems to be necessary - I don't know the specifics of why quite yet.
   server.handleClient();
 }
